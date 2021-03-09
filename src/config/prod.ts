cd554905f4bf7653c0d85d7414c8ba4cd5b20dc1
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-03-09 15:43:09 +0530

    added prod env variables

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -1,5 +1,7 @@
 module.exports = {
   mongoURI: process.env.mongoURI,
+  sendgridAPI: process.env.sendgridAPI,
+  forgotPasswordOTPTemplate: process.env.forgotPasswordOTPTemplate,
 };
 
 //mongodb+srv://vijay:V07Dm5Diwkz95eIj@cluster0.p1weg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
