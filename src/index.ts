a20e7446793a85f321fd09a5691de15383a22af4
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-03-10 10:51:14 +0530

    added env check

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,23 @@ const keys = require("./config/keys");
 import { app } from "./app";
 
 const start = async () => {
+  if (process.env.NODE_ENV) {
+    if (!process.env.mongoURI) {
+      throw new Error("MONGOURI must be defined");
+    }
+    if (!process.env.sendgridAPI) {
+      throw new Error("sendgridAPI must be defined");
+    }
+    if (!process.env.forgotPasswordOTPTemplate) {
+      throw new Error("forgotPasswordOTPTemplate must be defined");
+    }
+    if (!process.env.jwtKey) {
+      throw new Error("jwtKey must be defined");
+    }
+    if (!process.env.JWTEXPIRETIME) {
+      throw new Error("JWTEXPIRETIME must be defined");
+    }
+  }
   try {
     await mongoose.connect(keys.mongoURI, {
       useNewUrlParser: true,
